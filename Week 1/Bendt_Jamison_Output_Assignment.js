f6c66f6d8b4953db011a0dcc8baa96ba0b3818f6
JJamison <jmbendt@fullsail.edu>
2015-09-30 19:45:50 -0400

    added output

--- Week 1/Bendt_Jamison_Output_Assignment.js
+++ Week 1/Bendt_Jamison_Output_Assignment.js
@@ -8,3 +8,6 @@
 var myAge = (20);
 var myHobbies = ("My hobbies are playing video games, drawing, and talking to my friends. ");
 var playsGames = true;
+
+// outputs
+console.log("Hi my name is Jami " + myAge + myHobbies);
