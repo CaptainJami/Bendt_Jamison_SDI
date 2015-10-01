99678e032812b74de921fcb10f6e64aa8147f79e
JJamison <jmbendt@fullsail.edu>
2015-10-01 11:51:26 -0400

    revised code

--- Week 1/Bendt_Jamison_Output_Assignment.js
+++ Week 1/Bendt_Jamison_Output_Assignment.js
@@ -5,10 +5,10 @@
  */
 
 // variables
-var myAge = (20);
-var myHobbies = ("My hobbies are playing video games, drawing, and talking to my friends. ");
-var playsGames = true;
+var myHeight = (190.5); // Assigning a number variable
+var myHobbies = ("My hobbies are playing video games, drawing, and talking to my friends. "); // Assigning a string variable
+var playsGames = true; // Assigning a boolean variable
 
 // outputs
-console.log("Hi my name is Jami, I am "  + myAge + " years old. " + myHobbies);
-console.log(playsGames);
+console.log("Hi my name is Jami, I am "  + myHeight + " centimeters tall. " + myHobbies); // Outputting a string sentence, with string variable, and number variable
+console.log(playsGames); // Outputting a boolean variable
