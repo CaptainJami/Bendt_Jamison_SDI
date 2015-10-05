fda666e85a39975c81de6c183fdab5d85a7828b1
JJamison <jmbendt@fullsail.edu>
2015-10-04 22:17:41 -0400

    added variable

--- Week 2/Expressions_Worksheet/js/Expressions_Worksheet.js
+++ Week 2/Expressions_Worksheet/js/Expressions_Worksheet.js
@@ -9,7 +9,7 @@
 var humanYears = 4; // declared and assigned a variable
 var dogYears = (humanYears * 7); // declared and assigned a variable
 
-console.log("Sparky is " + humanYears + "in human years, and is " + dogYears + "in dog years. "); // assigned variables an output
+console.log("Sparky is " + humanYears + " in human years, and is " + dogYears + " in dog years. "); // assigned variables an output
 
     //Slice of pie, Part I
 var pizzasOrdered = 3; // declared and assigned a variable
@@ -17,10 +17,10 @@ var partySize = 7; // declared and assigned a variable
 var pizzaSlices = 12 * pizzasOrdered; // declared and assigned a variable
 var result = pizzaSlices / partySize; // declared and assigned a variable
 
-console.log("Each person ate " + result + "slices of pizza. "); // assigned variables an output
+console.log("Each person ate " + result + " slice(s) of pizza. "); // assigned variables an output
 
     //Slice of Pie, Part II
-console.log("Sparky got " + pizzaSlices % partySize + " slices of pizza."); // assigned variables a secondary output
+console.log("Sparky got " + pizzaSlices % partySize + " slice(s) of pizza."); // assigned variables a secondary output
 
     //Average Shopping Bill
 groceryTotals = [12, 4, 40, 64, 8]; // declared and assigned an array
@@ -29,4 +29,6 @@ var weeklyResult = totalResult / 5; // declared and assigned a variable
 
 console.log("You have spent a total of " + totalResult + " on groceries over 5 weeks. That would be an average of " + weeklyResult + " per week."); // assigned variables an output
 
+    //Discounts
+var originalPrice = 629.13; // declared and assigned a variable
 
