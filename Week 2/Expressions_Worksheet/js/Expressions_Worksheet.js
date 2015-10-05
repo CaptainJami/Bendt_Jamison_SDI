115faed156891a28518e1b64a8febb4fce5072ac
JJamison <jmbendt@fullsail.edu>
2015-10-04 20:48:35 -0400

    added variable

--- Week 2/Expressions_Worksheet/js/Expressions_Worksheet.js
+++ Week 2/Expressions_Worksheet/js/Expressions_Worksheet.js
@@ -12,4 +12,5 @@ var dogYears = (humanYears * 7); // declared and assigned a variable
 console.log("Sparky is " + humanYears + "in human years, and is " + dogYears + "in dog years. "); // assigned variables an output
 
     //Slice of pie, Part I
-var pizzasOrdered = 3;
+var pizzasOrdered = 3; // declared and assigned a variable
+var partySize = 7; // declared and assigned a variable 
