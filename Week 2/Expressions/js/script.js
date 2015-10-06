4ed3a6d6b5c1e0f8de5c7e0d1c3cf07a04cf6fe2
JJamison <jmbendt@fullsail.edu>
2015-10-06 09:14:40 -0400

    added variable and changed alert

--- Week 2/Expressions/js/script.js
+++ Week 2/Expressions/js/script.js
@@ -8,4 +8,5 @@ var height = prompt("Please enter the height.");
 var area = width * height; //calculates with that info
 //console.log(area); //prints it out to console
 
-alert("The area of your rectangle is " + area + " sq feet");
+var result = ("The area of your rectangle is " + area + " sq feet");
+alert(result);
