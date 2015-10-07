7dc581788c7008dd9929bef4c3825a7bc111b1da
JJamison <jmbendt@fullsail.edu>
2015-10-07 16:26:30 -0400

    fixed code

--- Week 2/Bendt_Jamison_Expressions_Assignment/js/Bendt_Jamison_Expressions_Assignment.js
+++ Week 2/Bendt_Jamison_Expressions_Assignment/js/Bendt_Jamison_Expressions_Assignment.js
@@ -5,11 +5,13 @@
 */
 
 // Variables
-var gamePrice; // declared a variable
-var gamesOwned; // declared a variable
-var favoriteGame; // declared a variable
-var totalPrice; // declared a variable
-var prices = []; // declared an array
+var gamePrice; // declared a number variable based on the price of games you own
+var gamesOwned; // declared a number variable on the number of games you own
+var favoriteGame; // declared a string variable for your favorite game
+var secondFavorite; // declared a string variable for your second favorite game
+var totalPrice; // declared a number variable of all games prices
+var prices = []; // declared an number array of prices
 
-prices[0] = prompt("Enter price of the first favorite game you own: ");
-prices[1] = prompt("Enter price of the second favorite game you own: ");
+prices[0] = prompt("Enter price of your favorite game: "); // assigned the array a prompt
+prices[1] = prompt("Enter price of your second favorite game: "); // assigned the array a prompt
+
