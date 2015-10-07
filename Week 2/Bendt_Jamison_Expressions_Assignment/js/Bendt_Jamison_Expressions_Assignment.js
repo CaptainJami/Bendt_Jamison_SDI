27f73e435c7c1327a266a9d78c97e863fc963316
JJamison <jmbendt@fullsail.edu>
2015-10-07 16:58:31 -0400

    added output

--- Week 2/Bendt_Jamison_Expressions_Assignment/js/Bendt_Jamison_Expressions_Assignment.js
+++ Week 2/Bendt_Jamison_Expressions_Assignment/js/Bendt_Jamison_Expressions_Assignment.js
@@ -17,10 +17,18 @@ prices[2] = prompt("Enter price of the third game on your list: " ); // assigned
 prices[3] = prompt("Enter price of the fourth game on your list: "); // assigned the array a prompt
 prices[4] = prompt("Enter price of the fifth and final game on your list: "); // assigned the array a prompt
 
+prices[0] = Number(prices[0]);
+prices[1] = Number(prices[1]);
+prices[2] = Number(prices[2]);
+prices[3] = Number(prices[3]);
+prices[4] = Number(prices[4]);
+
 favoriteGame = prompt("What is your favorite game?: "); //assigned string variable a prompt
 secondFavorite = prompt("What is your second favorite game?: "); //assigned a string variable a prompt
 
 gamesOwned = prompt("How many games do you own?: ");
 gamesOwned = Number(gamesOwned);
 
-var totalPrice = ([]);
+var totalPrice = prices[0] + prices[1] + prices[2] + prices[3] + prices[4];
+
+console.log("You have a total of " + gamesOwned + " games for a total price of $" + totalPrice + ", that is crazy I hope you enjoy them!");
