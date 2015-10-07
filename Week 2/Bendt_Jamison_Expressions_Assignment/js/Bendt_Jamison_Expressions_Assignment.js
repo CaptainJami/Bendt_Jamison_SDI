443aecb903f781991e27da625714b0b50c504fc4
JJamison <jmbendt@fullsail.edu>
2015-10-07 16:49:52 -0400

    revised code

--- Week 2/Bendt_Jamison_Expressions_Assignment/js/Bendt_Jamison_Expressions_Assignment.js
+++ Week 2/Bendt_Jamison_Expressions_Assignment/js/Bendt_Jamison_Expressions_Assignment.js
@@ -5,15 +5,17 @@
 */
 
 // Variables
-var gamePrice; // declared a number variable based on the price of games you own
 var gamesOwned; // declared a number variable on the number of games you own
 var favoriteGame; // declared a string variable for your favorite game
 var secondFavorite; // declared a string variable for your second favorite game
 var totalPrice; // declared a number variable of all games prices
 var prices = []; // declared an number array of prices
 
-prices[0] = prompt("Enter price of your favorite game: "); // assigned the array a prompt
-prices[1] = prompt("Enter price of your second favorite game: "); // assigned the array a prompt
+prices[0] = prompt("Enter price of the first game on your list: "); // assigned the array a prompt
+prices[1] = prompt("Enter price of the second game on your list: "); // assigned the array a prompt
+prices[2] = prompt("Enter price of the third game on your list: " ); // assigned the array a prompt
+prices[3] = prompt("Enter price of the fourth game on your list: "); // assigned the array a prompt
+prices[4] = prompt("Enter price of the fifth and final game on your list: "); // assigned the array a prompt
 
 favoriteGame = prompt("What is your favorite game?: "); //assigned string variable a prompt
 secondFavorite = prompt("What is your second favorite game?: "); //assigned a string variable a prompt
@@ -21,5 +23,4 @@ secondFavorite = prompt("What is your second favorite game?: "); //assigned a st
 gamesOwned = prompt("How many games do you own?: ");
 gamesOwned = Number(gamesOwned);
 
-gamePrice = prompt("The price of your most expensive game?: ");
-gamePrice = Numbers(gamePrice);
+var totalPrice = ([]);
