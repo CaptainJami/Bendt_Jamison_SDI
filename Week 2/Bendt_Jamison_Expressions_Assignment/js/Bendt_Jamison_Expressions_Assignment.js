18537a838465c859e5257a6a348e9a14723c9180
JJamison <jmbendt@fullsail.edu>
2015-10-07 17:20:09 -0400

    revised and added code

--- Week 2/Bendt_Jamison_Expressions_Assignment/js/Bendt_Jamison_Expressions_Assignment.js
+++ Week 2/Bendt_Jamison_Expressions_Assignment/js/Bendt_Jamison_Expressions_Assignment.js
@@ -11,24 +11,30 @@ var secondFavorite; // declared a string variable for your second favorite game
 var totalPrice; // declared a number variable of all games prices
 var prices = []; // declared an number array of prices
 
+
+alert("I would like to get to know you! ");
+favoriteGame = prompt("What is your favorite game?: "); //assigned string variable a prompt
+secondFavorite = prompt("What is your second favorite game?: "); //assigned a string variable a prompt
+alert("You have good taste in video games, and that is crazy I love those games!");
+console.log("You have good taste in video games, and that is crazy I love those games!");
+
+gamesOwned = prompt("How many games do you own? (we are doing 3 for the sake of the calculation) : ");
+gamesOwned = Number(gamesOwned);
+alert("Well we are just going to do at least 3 games, so get those prices ready and we will do some math!");
+
 prices[0] = prompt("Enter price of the first game on your list: "); // assigned the array a prompt
 prices[1] = prompt("Enter price of the second game on your list: "); // assigned the array a prompt
-prices[2] = prompt("Enter price of the third game on your list: " ); // assigned the array a prompt
-prices[3] = prompt("Enter price of the fourth game on your list: "); // assigned the array a prompt
-prices[4] = prompt("Enter price of the fifth and final game on your list: "); // assigned the array a prompt
+prices[2] = prompt("Enter price of the third and final game on your list: " ); // assigned the array a prompt
+
 
 prices[0] = Number(prices[0]);
 prices[1] = Number(prices[1]);
 prices[2] = Number(prices[2]);
-prices[3] = Number(prices[3]);
-prices[4] = Number(prices[4]);
 
-favoriteGame = prompt("What is your favorite game?: "); //assigned string variable a prompt
-secondFavorite = prompt("What is your second favorite game?: "); //assigned a string variable a prompt
 
-gamesOwned = prompt("How many games do you own?: ");
-gamesOwned = Number(gamesOwned);
+totalPrice = prices[0] + prices[1] + prices[2] + prices[3] + prices[4];
 
-var totalPrice = prices[0] + prices[1] + prices[2] + prices[3] + prices[4];
-
+alert("You have a total of " + gamesOwned + " games for a total price of $" + totalPrice + ", that is crazy I hope you enjoy them!");
 console.log("You have a total of " + gamesOwned + " games for a total price of $" + totalPrice + ", that is crazy I hope you enjoy them!");
+
+
