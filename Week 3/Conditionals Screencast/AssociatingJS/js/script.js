41f22f6b765918b2b33382d2e26542d1e72775d5
JJamison <jmbendt@fullsail.edu>
2015-10-13 11:06:55 -0400

    added conditionals screencasts

---
+++ Week 3/Conditionals Screencast/AssociatingJS/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Jamison Bendt
+ * SDI 1015
+ * Assignment Name
+ */
+
+temp = 65
+if(temp == 75){
+    goToTheBeach();
+}
+
+if(sunny){
+    goToTheBeach();
+
+    if(warmWater){
+        wearNewSuit();
+    }
+
+}else{
+    goToTheMovies();
+}
+
+//Conditional Logic
+
+var oldEnough = false;
+
+//if the child is old enough, print to the console
+if(oldEnough) {
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
