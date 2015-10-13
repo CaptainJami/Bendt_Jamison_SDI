6551083caad48e17f0dee75ede3f0ed3883785c3
JJamison <jmbendt@fullsail.edu>
2015-10-13 16:52:37 -0400

    added variable

--- Week 3/Conditionals_worksheet/AssociatingJS/js/script.js
+++ Week 3/Conditionals_worksheet/AssociatingJS/js/script.js
@@ -20,5 +20,5 @@ if(competitorsWeight >= 250 ){
 //variables
 var gasEfficiency = prompt("What is your cars miles per gallon?");
 var gaugeReading = prompt("What is the gas gauge reading? (in decimal percentage)");
-
+var gasCapacity = prompt("How many gallons of gas can your car hold?");
 
