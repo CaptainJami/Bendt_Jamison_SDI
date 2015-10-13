0357beacbdfcbb441142ef48edf88c89b660a1b7
JJamison <jmbendt@fullsail.edu>
2015-10-13 12:30:22 -0400

    starting example 1 for worksheet

---
+++ Week 3/Conditionals_worksheet/AssociatingJS/js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Jamison Bendt
+ * SDI 1015
+ * Assignment Name
+ */
+
+// Stuff your face
+
+var competitorsWeight = prompt();
+
+
+if(competitorsWeight >= 250 ){
+    console.log("The competitor qualifies for the heavyweight division.");
+}else{
+    console.log("The competitor needs to gain some weight!")
+}
