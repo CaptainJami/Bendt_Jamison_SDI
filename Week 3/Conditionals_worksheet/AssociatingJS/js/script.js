c5335d00a4dc5dd5196f4e75c96e25fc98697d87
JJamison <jmbendt@fullsail.edu>
2015-10-13 17:28:18 -0400

    added variable

--- Week 3/Conditionals_worksheet/AssociatingJS/js/script.js
+++ Week 3/Conditionals_worksheet/AssociatingJS/js/script.js
@@ -6,7 +6,7 @@
 
 // Stuff your face
 
-    variables
+// variable(s)
 var competitorsWeight = prompt("What is your weight? (in pounds)");
 
 if(competitorsWeight >= 250 ){
@@ -17,10 +17,10 @@ if(competitorsWeight >= 250 ){
 
 // Last Chance for Gas!
 
-//variables
-var gasEfficiency = prompt("What is your cars miles per gallon?");
-var gaugeReading = prompt("What is the gas gauge reading? (in decimal percentage)");
-var gasCapacity = prompt("How many gallons of gas can your car hold?");
+//variable(s)
+var gasEfficiency = prompt("What is your cars miles per gallon?"); //declared and assigned a variable with a prompt asking for user data
+var gaugeReading = prompt("What is the gas gauge reading? (in decimal percentage)"); //declared and assigned a variable with a prompt asking for user data
+var gasCapacity = prompt("How many gallons of gas can your car hold?"); //declared and assigned a variable with a prompt asking for user data
 var gallonsLeft;
 
 gasEfficiency = Number(gasEfficiency);
@@ -28,3 +28,19 @@ gaugeReading = Number(gaugeReading);
 gasCapacity = Number(gasCapacity);
 
 gallonsLeft = gasEfficiency * gaugeReading + gasCapacity;
+
+if(gallonsLeft >= 200 / gasEfficiency) {
+    console.log("Yes you can make it without stopping for gas!");
+}else{
+    console.log("You only have " + gallonsLeft + " gallons of gas in your tank, better get gas now while you can!");
+}
+
+// Grade Letter Calculator
+
+//variable(s)
+var currentGrade = prompt("What is your number grade currently in this class? (rounded to nearest number)");
+
+
+currentGrade = Number(currentGrade);
+
+}
