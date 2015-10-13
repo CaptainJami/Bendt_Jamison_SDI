c9bbca29b25cc6dec8d0194df08d0ab8cb1277f9
JJamison <jmbendt@fullsail.edu>
2015-10-13 11:10:34 -0400

    added conditionals

--- Week 3/Conditionals Screencast/AssociatingJS/js/script.js
+++ Week 3/Conditionals Screencast/AssociatingJS/js/script.js
@@ -29,3 +29,15 @@ if(oldEnough) {
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }
+
+//Conditional Logic - Relational Expressions
+
+var kidHeight = 40;
+var minHeight = 48;
+
+//if the child is old enough, print to the console "you can ride!"
+//if kid is over 48 inches in height
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
