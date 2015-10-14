f4050c3594a6e8593e25f2f6dec134a4145f37ee
JJamison <jmbendt@fullsail.edu>
2015-10-14 00:53:19 -0400

    added variable

--- Week 3/Bendt_Jamison_Conditionals_Assignment/AssociatingJS/js/script.js
+++ Week 3/Bendt_Jamison_Conditionals_Assignment/AssociatingJS/js/script.js
@@ -7,3 +7,4 @@
 var budget = prompt("What is your budget for candy?");
 var candyBarPrice1 = prompt("What is the price of the first candy bar?");
 var candyBarPrice2 = prompt("What is the price of the second candy bar?");
+var salesTax = 0.06;
