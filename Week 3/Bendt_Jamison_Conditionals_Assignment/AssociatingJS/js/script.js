62729cd5882ede8ad0b2110b33a6b633a8d1aa8e
JJamison <jmbendt@fullsail.edu>
2015-10-14 00:58:04 -0400

    added variable

--- Week 3/Bendt_Jamison_Conditionals_Assignment/AssociatingJS/js/script.js
+++ Week 3/Bendt_Jamison_Conditionals_Assignment/AssociatingJS/js/script.js
@@ -16,3 +16,4 @@ var candyBarPrice1Total = (candyBarPrice1 * salesTax) + candyBarPrice1;
 var candyBarPrice2Total = (candyBarPrice2 * salesTax) + candyBarPrice2;
 var candyBarTax = (candyBarPrice1 + candyBarPrice2) * salesTax;
 var candyBarTotal = candyBarTax + candyBarPrice1 + candyBarPrice2;
+var totalPrice = candyBarTotal * salesTax;
