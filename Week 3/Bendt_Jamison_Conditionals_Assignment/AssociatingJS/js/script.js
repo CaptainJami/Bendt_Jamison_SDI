b3b081c55911bc943f3160c5415169367bcddd77
JJamison <jmbendt@fullsail.edu>
2015-10-14 00:54:19 -0400

    declared string variables as a number

--- Week 3/Bendt_Jamison_Conditionals_Assignment/AssociatingJS/js/script.js
+++ Week 3/Bendt_Jamison_Conditionals_Assignment/AssociatingJS/js/script.js
@@ -8,3 +8,6 @@ var budget = prompt("What is your budget for candy?");
 var candyBarPrice1 = prompt("What is the price of the first candy bar?");
 var candyBarPrice2 = prompt("What is the price of the second candy bar?");
 var salesTax = 0.06;
+
+candyBarPrice1 = Number(candyBarPrice1);
+candyBarPrice2 = Number(candyBarPrice2);
