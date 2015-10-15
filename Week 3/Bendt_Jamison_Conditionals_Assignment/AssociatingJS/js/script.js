0d457a0d4c74598c15aaf3ed4835eb15e3561fbd
JJamison <jmbendt@fullsail.edu>
2015-10-14 22:07:37 -0400

    fixed code and added a prompt validation

--- Week 3/Bendt_Jamison_Conditionals_Assignment/AssociatingJS/js/script.js
+++ Week 3/Bendt_Jamison_Conditionals_Assignment/AssociatingJS/js/script.js
@@ -5,23 +5,6 @@
  */
 
 var station = prompt("Is the gas station open?");
-var budget = prompt("What is your budget for candy?");
-var candyBarPrice1 = prompt("What is the price of the first candy bar?");
-var candyBarPrice2 = prompt("What is the price of the second candy bar?");
-var salesTax = 0.06;
-
-candyBarPrice1 = Number(candyBarPrice1);
-candyBarPrice2 = Number(candyBarPrice2);
-
-var candyBarPrice1Total = (candyBarPrice1 * salesTax) + candyBarPrice1;
-var candyBarPrice2Total = (candyBarPrice2 * salesTax) + candyBarPrice2;
-var candyBarTax = (candyBarPrice1 + candyBarPrice2) * salesTax;
-var candyBarTotal = candyBarTax + candyBarPrice1 + candyBarPrice2;
-var totalPrice = (candyBarTotal * salesTax) + candyBarTotal;
-var leftOver1 = budget - candyBarPrice1Total;
-var leftOver2 = budget - candyBarPrice2Total;
-var neededAmount = candyBarTotal - (candyBarPrice1 + candyBarPrice2);
-var price = budget - totalPrice;
 
 if(station == "Yes" || station == "yes"){
     station = true;
@@ -32,6 +15,37 @@ if(station == "Yes" || station == "yes"){
 var gasStation = (station === true) ? "The station is open, lets get some candy!" : "The station is closed, I guess I will wait until tomorrow.";
 
 console.log(gasStation);
+
+var budget = prompt("What is your budget for candy?");
+
+if(budget === ""){
+    budget = prompt("Please enter a valid number.");
+    console.log("Let's hope " + budget + " is enough for this delicious candy.");
+}else{
+    console.log("Let's hope " + budget + " is enough for this delicious candy.");
+}
+budget = Number(budget);
+
+var candyBarPrice1 = Number(prompt("What is the price of the first candy bar?"));
+
+
+
+var candyBarPrice2 = Number(prompt("What is the price of the second candy bar?"));
+
+
+
+
+var salesTax = 0.06;
+var candyBarPrice1Total = (candyBarPrice1 * salesTax) + candyBarPrice1;
+var candyBarPrice2Total = (candyBarPrice2 * salesTax) + candyBarPrice2;
+var candyBarTax = (candyBarPrice1 + candyBarPrice2) * salesTax;
+var candyBarTotal = candyBarTax + candyBarPrice1 + candyBarPrice2;
+var totalPrice = (candyBarTotal * salesTax) + candyBarTotal;
+var leftOver1 = budget - candyBarPrice1Total;
+var leftOver2 = budget - candyBarPrice2Total;
+var neededAmount = candyBarTotal - (candyBarPrice1 + candyBarPrice2);
+var price = budget - totalPrice;
+
 
 if(candyBarPrice1Total <= budget || candyBarPrice2Total <= budget){
     console.log("You have enough money for only the first candy bar with $" + leftOver1 + " left over.");
