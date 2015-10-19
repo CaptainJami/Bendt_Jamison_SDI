086f8ef4b8fc64c489c9bc6312d3b6285aadd5da
JJamison <jmbendt@fullsail.edu>
2015-10-19 11:03:25 -0400

    doing screencast

--- Week 4/Function Screencast/AssociatingJS/js/script.js
+++ Week 4/Function Screencast/AssociatingJS/js/script.js
@@ -9,9 +9,16 @@ function outputMsg(){
     console.log("Hello World");
 }
 
+var width = 5;
+
 function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
     console.log(area);
 }
+
+calcArea();
+calcArea();
+calcArea();
+console.log(width);
