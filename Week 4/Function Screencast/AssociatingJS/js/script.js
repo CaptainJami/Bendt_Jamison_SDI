c9710f6fa09875378df2830573bd558e79973a28
JJamison <jmbendt@fullsail.edu>
2015-10-19 11:21:36 -0400

    following screencast

--- Week 4/Function Screencast/AssociatingJS/js/script.js
+++ Week 4/Function Screencast/AssociatingJS/js/script.js
@@ -30,3 +30,34 @@ function dogYears(age){ // parameters
 var age1 = 4; // Arguments
 dogYears(age1);
 dogYears(5);
+
+
+{
+    var total = calcArea(30, 20);
+
+    function calcArea2(w, h) {
+        var area2 = w * h;
+        return area2; //function spitting the info
+    }
+
+    console.log(total);
+}
+
+function calcAreaF(width, height){
+    var areaF = width * height;
+    return areaF;
+}
+
+function calcAreaP(width, height){ // procedure
+    var areaF = width* height;
+    console.log(areaF);
+}
+
+var calcAreaS = function(width, height){
+    var areaS = width * height;
+    return areaS;
+};
+
+var a = calcAreaS(20, 30);
+
+console.log(a);
