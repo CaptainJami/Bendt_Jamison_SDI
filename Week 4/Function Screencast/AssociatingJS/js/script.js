048541b5480e82ad00267014b73f9c0368d52b1d
JJamison <jmbendt@fullsail.edu>
2015-10-19 10:34:59 -0400

    added function to screencast

--- Week 4/Function Screencast/AssociatingJS/js/script.js
+++ Week 4/Function Screencast/AssociatingJS/js/script.js
@@ -8,3 +8,10 @@
 function outputMsg(){
     console.log("Hello World");
 }
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
